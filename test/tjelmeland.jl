a3bdbeb7e1c1f27abbc67430619b533a2e4d0557
Oliver Schulz <oschulz@mpp.mpg.de>
2018-01-31 16:41:35 +0100

    Rewrite of multipropT2 and helper functions

--- test/tjelmeland.jl
+++ test/tjelmeland.jl
@@ -4,21 +4,10 @@ using BAT
 using Compat.Test
 
 @testset "tjelmeland" begin
-    # Computed with pencil and paper
-    input = [288, 64, 135] / 487
-    κ = 3 # row of interest
-    @test multipropT2(input, κ) ≈ [359, 64, 0] / 423
+    @test BAT.multipropT2([135, 288, 64] / 487) ≈ [0, 359, 64] / 423
+    @test BAT.multipropT2([0.2, 0.5, 0.3]) ≈ [0.0, 0.625, 0.375]
+    @test sum(BAT.multipropT2([123, 12, 324, 3, 31] / 493)) ≈ 1.
 
-    input2 = [0.2, 0.5, 0.3]
-    k2 = 3
-    @test multipropT2(input2, k2) ≈ [0.25, 0.75, 0.0]
-
-    input3 = [324, 3, 31, 123, 12]
-    k3 = 3
-    output = multipropT2(input3, k3)
-    @test sum(output) ≈ 1.
-
-    # invalid input should lead to errors
-    @test_throws ArgumentError multipropT2(input, -2)
-    @test_throws ArgumentError multipropT2(-input, 2)
+    @test_throws ArgumentError BAT.multipropT2([-0.1, 0.1])
+    @test_throws ArgumentError BAT.multipropT2([0.1, 0.8])
 end
